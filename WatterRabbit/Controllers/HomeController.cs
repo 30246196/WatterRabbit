04e901ea1c7f484a3173312df731805165e38da7
30246196 <edithserra1@gmail.com>
2026-03-22 22:54:34 +0000

    Add project files.

---
+++ WatterRabbit/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WatterRabbit.Controllers
+{
+    public class HomeController : Controller
+    {
+        //added
+        public ActionResult SplashScreen()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WatterRabbit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WatterRabbit.MvcApplication" Language="C#" %>
