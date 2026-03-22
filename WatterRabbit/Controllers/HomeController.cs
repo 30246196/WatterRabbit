dabd01239437b614b0e4d89d832f9df8e735a357
30246196 <edithserra1@gmail.com>
2026-03-22 23:28:44 +0000

    Task 1: Creating the Splash Screen. Added SplashScreen.cshtml view with video background from HomeController (Views/Home). Custom styles and navigation link in SplahScreen.cshtml.

--- WatterRabbit/Controllers/HomeController.cs
+++ WatterRabbit/Controllers/HomeController.cs
@@ -8,19 +8,24 @@ namespace WatterRabbit.Controllers
 {
     public class HomeController : Controller
     {
-        //added
+        //added SplashScreen action to show the splash screen when the application starts
+        // Displays the Splash Screen created in Task 1.
+        // This view is rendered without a layout and contains the video animation.
         public ActionResult SplashScreen()
         {
             return View();
         }
 
+        // Displays the main home page of the application.
         public ActionResult Index()
         {
             return View();
         }
 
+        // Displays the About page, which explains the purpose of the app.
         public ActionResult About()
         {
+            // Passes a short message to the view using ViewBag.
             ViewBag.Message = "Your application description page.";
 
             return View();
